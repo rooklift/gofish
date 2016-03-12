a5f2e7e68699f31fa6a4229329c0fe231190c3c9
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-03-12 16:50:40 +0000

    main()

--- sgf_pygame.py
+++ sgf_pygame.py
@@ -67,147 +67,154 @@ def blit_without_adjust(target, source, x, y):
 	target.blit(source, (x, y))
 
 
-# ----------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
 
-# Load the game...
 
-node = sgf.load(sys.argv[1])
-node.print_comments()
+def main():
 
-# Patch up the board with the grid and hoshi points drawn...
+	# Load the game...
 
-for n in range(1, node.board.boardsize + 1):
-	pygame.draw.line(spriteGoban, pygame.Color(0, 0, 0), (n * GAP, GAP), (n * GAP, node.board.boardsize * GAP), 1)
-	pygame.draw.line(spriteGoban, pygame.Color(0, 0, 0), (GAP, n * GAP), (node.board.boardsize * GAP, n * GAP), 1)
+	node = sgf.load(sys.argv[1])
+	node.print_comments()
 
-for x in range(3, node.board.boardsize - 1):
-	for y in range(node.board.boardsize - 1):
-		if sgf.is_star_point(x, y, node.board.boardsize):
-			blit(spriteGoban, spriteHoshi, x * GAP, y * GAP)
+	# Patch up the board with the grid and hoshi points drawn...
 
-while 1:
+	for n in range(1, node.board.boardsize + 1):
+		pygame.draw.line(spriteGoban, pygame.Color(0, 0, 0), (n * GAP, GAP), (n * GAP, node.board.boardsize * GAP), 1)
+		pygame.draw.line(spriteGoban, pygame.Color(0, 0, 0), (GAP, n * GAP), (node.board.boardsize * GAP, n * GAP), 1)
 
-	# Update keyboard states...
+	for x in range(3, node.board.boardsize - 1):
+		for y in range(node.board.boardsize - 1):
+			if sgf.is_star_point(x, y, node.board.boardsize):
+				blit(spriteGoban, spriteHoshi, x * GAP, y * GAP)
 
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			cleanexit()
-		if event.type == KEYDOWN:
-			keyboard[event.key] = 1
-		if event.type == KEYUP:
-			keyboard[event.key] = 0
+	while 1:
 
-	# Handle input if a key is down. Set the key to be up to avoid repetitions...
+		# Update keyboard states...
 
-	if keyboard.get(K_DOWN, 0) or keyboard.get(K_RIGHT, 0):
-		keyboard[K_DOWN] = 0
-		keyboard[K_RIGHT] = 0
-		try:
-			node = node.children[0]
-			node.print_comments()
-		except IndexError:
-			pass
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				cleanexit()
+			if event.type == KEYDOWN:
+				keyboard[event.key] = 1
+			if event.type == KEYUP:
+				keyboard[event.key] = 0
 
-	if keyboard.get(K_UP, 0) or keyboard.get(K_LEFT, 0):
-		keyboard[K_UP] = 0
-		keyboard[K_LEFT] = 0
-		if node.parent:
-			node = node.parent
+		# Handle input if a key is down. Set the key to be up to avoid repetitions...
 
-	if keyboard.get(K_PAGEDOWN, 0):
-		keyboard[K_PAGEDOWN] = 0
-		for n in range(10):
+		if keyboard.get(K_DOWN, 0) or keyboard.get(K_RIGHT, 0):
+			keyboard[K_DOWN] = 0
+			keyboard[K_RIGHT] = 0
 			try:
 				node = node.children[0]
 				node.print_comments()
 			except IndexError:
-				break
+				pass
 
-	if keyboard.get(K_PAGEUP, 0):
-		keyboard[K_PAGEUP] = 0
-		for n in range(10):
+		if keyboard.get(K_UP, 0) or keyboard.get(K_LEFT, 0):
+			keyboard[K_UP] = 0
+			keyboard[K_LEFT] = 0
 			if node.parent:
 				node = node.parent
-			else:
-				break
 
-	if keyboard.get(K_TAB, 0):
-		keyboard[K_TAB] = 0
+		if keyboard.get(K_PAGEDOWN, 0):
+			keyboard[K_PAGEDOWN] = 0
+			for n in range(10):
+				try:
+					node = node.children[0]
+					node.print_comments()
+				except IndexError:
+					break
+
+		if keyboard.get(K_PAGEUP, 0):
+			keyboard[K_PAGEUP] = 0
+			for n in range(10):
+				if node.parent:
+					node = node.parent
+				else:
+					break
+
+		if keyboard.get(K_TAB, 0):
+			keyboard[K_TAB] = 0
+			if node.parent:
+				if len(node.parent.children) > 1:
+					index = node.parent.children.index(node)
+					if index < len(node.parent.children) - 1:
+						index += 1
+					else:
+						index = 0
+					node = node.parent.children[index]
+					node.print_comments()
+
+		if keyboard.get(K_BACKSPACE, 0):		# Return to the main line
+			keyboard[K_BACKSPACE] = 0
+			while 1:
+				if node.is_main_line:
+					break
+				if node.parent is None:
+					break
+				node = node.parent
+
+		if keyboard.get(K_HOME, 0):
+			keyboard[K_HOME] = 0
+			node = node.get_root_node()
+
+		if keyboard.get(K_END, 0):
+			keyboard[K_END] = 0
+			node = node.get_end_node()
+
+		# Set the title...
+
+		title = "Move {}".format(node.moves_made)
 		if node.parent:
 			if len(node.parent.children) > 1:
 				index = node.parent.children.index(node)
-				if index < len(node.parent.children) - 1:
-					index += 1
-				else:
-					index = 0
-				node = node.parent.children[index]
-				node.print_comments()
+				title += " ({} of {} variations)".format(index + 1, len(node.parent.children))
+		pygame.display.set_caption(title)
 
-	if keyboard.get(K_BACKSPACE, 0):		# Return to the main line
-		keyboard[K_BACKSPACE] = 0
-		while 1:
-			if node.is_main_line:
-				break
-			if node.parent is None:
-				break
-			node = node.parent
+		# Draw the board...
 
-	if keyboard.get(K_HOME, 0):
-		keyboard[K_HOME] = 0
-		node = node.get_root_node()
+		blit_without_adjust(virtue, spriteGoban, 0, 0)
 
-	if keyboard.get(K_END, 0):
-		keyboard[K_END] = 0
-		node = node.get_end_node()
+		# Draw the stones...
 
-	# Set the title...
+		for x in range(1, node.board.boardsize + 1):
+			for y in range(1, node.board.boardsize + 1):
+				if node.board.state[x][y] == sgf.BLACK:
+					blit(virtue, spriteBlack, GAP * x, GAP * y)
+				elif node.board.state[x][y] == sgf.WHITE:
+					blit(virtue, spriteWhite, GAP * x, GAP * y)
 
-	title = "Move {}".format(node.moves_made)
-	if node.parent:
-		if len(node.parent.children) > 1:
-			index = node.parent.children.index(node)
-			title += " ({} of {} variations)".format(index + 1, len(node.parent.children))
-	pygame.display.set_caption(title)
+		# Draw a mark at the current move, if there is one...
 
-	# Draw the board...
+		move = node.what_was_the_move()
+		if move is not None:
+			mark_x = move[0]
+			mark_y = move[1]
+			blit(virtue, spriteMove, GAP * mark_x, GAP * mark_y)
 
-	blit_without_adjust(virtue, spriteGoban, 0, 0)
+		# Draw a mark at variations, if there are any...
 
-	# Draw the stones...
+		for sib_move in node.sibling_moves():
+			mark_x = sib_move[0]
+			mark_y = sib_move[1]
+			blit(virtue, spriteVar, GAP * mark_x, GAP * mark_y)
 
-	for x in range(1, node.board.boardsize + 1):
-		for y in range(1, node.board.boardsize + 1):
-			if node.board.state[x][y] == sgf.BLACK:
-				blit(virtue, spriteBlack, GAP * x, GAP * y)
-			elif node.board.state[x][y] == sgf.WHITE:
-				blit(virtue, spriteWhite, GAP * x, GAP * y)
+		# Draw the commonly used marks...
 
-	# Draw a mark at the current move, if there is one...
+		for mark in markup_dict:
+			if mark in node.properties:
+				points = set()
+				for value in node.properties[mark]:
+					points |= sgf.points_from_points_string(value, node.board.boardsize)
+				for point in points:
+					blit(virtue, markup_dict[mark], GAP * point[0], GAP * point[1])
 
-	move = node.what_was_the_move()
-	if move is not None:
-		mark_x = move[0]
-		mark_y = move[1]
-		blit(virtue, spriteMove, GAP * mark_x, GAP * mark_y)
+		# Update and wait...
 
-	# Draw a mark at variations, if there are any...
+		pygame.display.update()
+		fpsClock.tick(30)
 
-	for sib_move in node.sibling_moves():
-		mark_x = sib_move[0]
-		mark_y = sib_move[1]
-		blit(virtue, spriteVar, GAP * mark_x, GAP * mark_y)
 
-	# Draw the commonly used marks...
-
-	for mark in markup_dict:
-		if mark in node.properties:
-			points = set()
-			for value in node.properties[mark]:
-				points |= sgf.points_from_points_string(value, node.board.boardsize)
-			for point in points:
-				blit(virtue, markup_dict[mark], GAP * point[0], GAP * point[1])
-
-	# Update and wait...
-
-	pygame.display.update()
-	fpsClock.tick(30)
+if __name__ == "__main__":
+	main()
