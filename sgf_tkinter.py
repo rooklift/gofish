f9b1140bf595117e51b916a89a21c150e9035ea1
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-03-14 23:58:05 +0000

    drastic switch to class-based structure

--- sgf_tkinter.py
+++ sgf_tkinter.py
@@ -6,7 +6,6 @@ import sgf
 WIDTH, HEIGHT = 621, 621
 GAP = 31
 
-
 MOTD = """
   Fohristiwhirl's SGF readwriter. Keys:
 
@@ -20,6 +19,8 @@ MOTD = """
   -- MAKE MOVE: Mouse Button
 """
 
+# --------------------------------------------------------------------------------------
+# Various utility functions...
 
 def load_graphics():
     directory = os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -41,6 +42,8 @@ def load_graphics():
 
     global markup_dict; markup_dict = {"TR": spriteTriangle, "CR": spriteCircle, "SQ": spriteSquare, "MA": spriteMark}
 
+# --------------------------------------------------------------------------------------
+# Currently everything that returns a value is outside the class...
 
 def screen_pos_from_board_pos(x, y, boardsize):
     gridsize = GAP * (boardsize - 1) + 1
@@ -49,76 +52,12 @@ def screen_pos_from_board_pos(x, y, boardsize):
     ret_y = (y - 1) * GAP + margin
     return ret_x, ret_y
 
-
 def board_pos_from_screen_pos(x, y, boardsize):        # Inverse of the above
     gridsize = GAP * (boardsize - 1) + 1
     margin = (WIDTH - gridsize) // 2
     ret_x = round((x - margin) / GAP + 1)
     ret_y = round((y - margin) / GAP + 1)
     return ret_x, ret_y
-
-
-def draw_node(canvas, node):
-    canvas.delete(tkinter.ALL)              # DESTROY all!
-    boardsize = node.board.boardsize
-
-    # Draw the texture...
-
-    canvas.create_image(0, 0, anchor = tkinter.NW, image = spriteTexture)
-
-    # Draw the hoshi points...
-
-    for x in range(3, boardsize - 1):
-        for y in range(boardsize - 1):
-            if sgf.is_star_point(x, y, boardsize):
-                screen_x, screen_y = screen_pos_from_board_pos(x, y, boardsize)
-                canvas.create_image(screen_x, screen_y, image = spriteHoshi)
-
-    # Draw the lines...
-
-    for n in range(1, boardsize + 1):
-        start_a, start_b = screen_pos_from_board_pos(n, 1, boardsize)
-        end_a, end_b = screen_pos_from_board_pos(n, boardsize, boardsize)
-
-        end_b += 1
-
-        canvas.create_line(start_a, start_b, end_a, end_b)
-        canvas.create_line(start_b, start_a, end_b, end_a)
-
-    # Draw the stones...
-
-    for x in range(1, node.board.boardsize + 1):
-        for y in range(1, node.board.boardsize + 1):
-            screen_x, screen_y = screen_pos_from_board_pos(x, y, node.board.boardsize)
-            if node.board.state[x][y] == sgf.BLACK:
-                canvas.create_image(screen_x, screen_y, image = spriteBlack)
-            elif node.board.state[x][y] == sgf.WHITE:
-                canvas.create_image(screen_x, screen_y, image = spriteWhite)
-
-    # Draw a mark at the current move, if there is one...
-
-    move = node.what_was_the_move()
-    if move is not None:
-        screen_x, screen_y = screen_pos_from_board_pos(move[0], move[1], node.board.boardsize)
-        canvas.create_image(screen_x, screen_y, image = spriteMove)
-
-    # Draw a mark at variations, if there are any...
-
-    for sib_move in node.sibling_moves():
-        screen_x, screen_y = screen_pos_from_board_pos(sib_move[0], sib_move[1], node.board.boardsize)
-        canvas.create_image(screen_x, screen_y, image = spriteVar)
-
-    # Draw the commonly used marks...
-
-    for mark in markup_dict:
-        if mark in node.properties:
-            points = set()
-            for value in node.properties[mark]:
-                points |= sgf.points_from_points_string(value, node.board.boardsize)
-            for point in points:
-                screen_x, screen_y = screen_pos_from_board_pos(point[0], point[1], node.board.boardsize)
-                canvas.create_image(screen_x, screen_y, image = markup_dict[mark])
-
 
 def title_bar_string(node):
     title = "Move {}".format(node.moves_made)
@@ -128,179 +67,226 @@ def title_bar_string(node):
             title += " ({} of {} variations)".format(index + 1, len(node.parent.children))
     return title
 
+# --------------------------------------------------------------------------------------
 
-# All the key handlers are in the same form:
-#
-# def handle_key_NAME(node):
-#     <do stuff>
-#     return node
-#
-# where NAME is an uppercase version of the event.keysym, see:
-# http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/key-names.html
-#
-# One can make a new key handler just by creating it, no other work is needed anywhere
+class BoardCanvas(tkinter.Canvas):
+    def __init__(self, owner, filename, *args, **kwargs):
+        tkinter.Canvas.__init__(self, owner, *args, **kwargs)
 
+        self.owner = owner
 
-def handle_key_DOWN(node):
-    try:
-        node = node.children[0]
-        node.print_comments()
-    except IndexError:
-        pass
-    return node
+        self.bind("<Key>", self.call_keypress_handler)
+        self.bind("<Button-1>", self.mouseclick_handler)
+        self.bind("<Control-o>", self.opener)
+        self.bind("<Control-s>", self.saver)
 
-def handle_key_RIGHT(node):
-    return handle_key_DOWN(node)
+        self.node = sgf.new_tree(19)    # Do this now in case the load fails
 
-def handle_key_UP(node):
-    if node.parent:
-        node = node.parent
-    return node
+        if filename is not None:
+            self.open_file(filename)
 
-def handle_key_LEFT(node):
-    return handle_key_UP(node)
+        self.draw_node()
 
-def handle_key_NEXT(node):          # PageDown
-    for n in range(10):
+    def open_file(self, infilename):
         try:
-            node = node.children[0]
-            node.print_comments()
-        except IndexError:
-            break
-    return node
-
-def handle_key_PRIOR(node):         # PageUp
-    for n in range(10):
-        if node.parent:
-            node = node.parent
-        else:
-            break
-    return node
-
-def handle_key_TAB(node):
-    if node.parent:
-        if len(node.parent.children) > 1:
-            index = node.parent.children.index(node)
-            if index < len(node.parent.children) - 1:
-                index += 1
-            else:
-                index = 0
-            node = node.parent.children[index]
-            node.print_comments()
-    return node
-
-def handle_key_BACKSPACE(node):         # Return to the main line
-    while 1:
-        if node.is_main_line:
-            break
-        if node.parent is None:
-            break
-        node = node.parent
-    return node
-
-def handle_key_HOME(node):
-    return node.get_root_node()
-
-def handle_key_END(node):
-    return node.get_end_node()
-
-def handle_key_DELETE(node):
-    if len(node.children) > 0:
-        ok = tkinter.messagebox.askokcancel("Delete?", "Delete this node and all of its children?")
-    else:
-        ok = True
-    if ok:
-        if node.parent:
-            child = node
-            node = node.parent
-            node.children.remove(child)
-            node.fix_main_line_status_recursive()
-        else:
-            node = sgf.new_tree(19)
-    return node
-
-def handle_key_D(node):
-    node.debug()
-    return node
-
-# Other handlers...
-
-def opener(node):
-    infilename = tkinter.filedialog.askopenfilename()
-    if infilename:
-        try:
-            node = sgf.load(infilename)
+            self.node = sgf.load(infilename)
             print("<--- Loaded: {}\n".format(infilename))
-            node.dump(include_comments = False)
+            self.node.dump(include_comments = False)
             print()
-            node.print_comments()
+            self.node.print_comments()
         except FileNotFoundError:
             print("error while loading: file not found")
         except sgf.BadBoardSize:
             print("error while loading: SZ (board size) was not in range 1:19")
         except sgf.ParserFail:
             print("error while loading: parser failed (invalid SGF?)")
-    return node
 
-def saver(node):
-    outfilename = tkinter.filedialog.asksaveasfilename(defaultextension=".sgf")
-    if outfilename:
-        sgf.save_file(outfilename, node)
-        print("---> Saved: {}\n".format(outfilename))
-    return node
+    def draw_node(self):
+        self.delete(tkinter.ALL)              # DESTROY all!
+        boardsize = self.node.board.boardsize
 
-def mouseclick_handler(node, x, y):
-    result = node.try_move(x, y)
-    if result:
-        node = result
-    return node
+        # Fix the title bar...
 
+        self.owner.wm_title(title_bar_string(self.node))
 
+        # Draw the texture...
 
-def main():
-    print(MOTD)
+        self.create_image(0, 0, anchor = tkinter.NW, image = spriteTexture)
 
-    try:
-        node = sgf.load(sys.argv[1])
-        print("<--- Loaded: {}\n".format(sys.argv[1]))
-        node.dump(include_comments = False)
-        print()
-        node.print_comments()
-    except (IndexError, FileNotFoundError):
-        node = sgf.new_tree(19)
+        # Draw the hoshi points...
 
-    # We define some functions to call the handlers with the argument they
-    # really need ("node") and then draw the window; also updating the
-    # almost-local variable "node" from the return value of the handler
+        for x in range(3, boardsize - 1):
+            for y in range(boardsize - 1):
+                if sgf.is_star_point(x, y, boardsize):
+                    screen_x, screen_y = screen_pos_from_board_pos(x, y, boardsize)
+                    self.create_image(screen_x, screen_y, image = spriteHoshi)
 
-    def call_keypress_handler(event):
-        nonlocal node
+        # Draw the lines...
+
+        for n in range(1, boardsize + 1):
+            start_a, start_b = screen_pos_from_board_pos(n, 1, boardsize)
+            end_a, end_b = screen_pos_from_board_pos(n, boardsize, boardsize)
+
+            end_b += 1
+
+            self.create_line(start_a, start_b, end_a, end_b)
+            self.create_line(start_b, start_a, end_b, end_a)
+
+        # Draw the stones...
+
+        for x in range(1, self.node.board.boardsize + 1):
+            for y in range(1, self.node.board.boardsize + 1):
+                screen_x, screen_y = screen_pos_from_board_pos(x, y, self.node.board.boardsize)
+                if self.node.board.state[x][y] == sgf.BLACK:
+                    self.create_image(screen_x, screen_y, image = spriteBlack)
+                elif self.node.board.state[x][y] == sgf.WHITE:
+                    self.create_image(screen_x, screen_y, image = spriteWhite)
+
+        # Draw a mark at the current move, if there is one...
+
+        move = self.node.what_was_the_move()
+        if move is not None:
+            screen_x, screen_y = screen_pos_from_board_pos(move[0], move[1], self.node.board.boardsize)
+            self.create_image(screen_x, screen_y, image = spriteMove)
+
+        # Draw a mark at variations, if there are any...
+
+        for sib_move in self.node.sibling_moves():
+            screen_x, screen_y = screen_pos_from_board_pos(sib_move[0], sib_move[1], self.node.board.boardsize)
+            self.create_image(screen_x, screen_y, image = spriteVar)
+
+        # Draw the commonly used marks...
+
+        for mark in markup_dict:
+            if mark in self.node.properties:
+                points = set()
+                for value in self.node.properties[mark]:
+                    points |= sgf.points_from_points_string(value, self.node.board.boardsize)
+                for point in points:
+                    screen_x, screen_y = screen_pos_from_board_pos(point[0], point[1], self.node.board.boardsize)
+                    self.create_image(screen_x, screen_y, image = markup_dict[mark])
+
+    # --------------------------------------------------------------------------------------
+    # All the key handlers are in the same form:
+    #
+    # def handle_key_NAME(self):
+    #     <do stuff>
+    #
+    # where NAME is an uppercase version of the event.keysym, see:
+    # http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/key-names.html
+    #
+    # One can make a new key handler just by creating it, no other work is needed anywhere
+
+    def call_keypress_handler(self, event):
         try:
-            function_name = "handle_key_" + event.keysym.upper()
-            node = globals()[function_name](node)
-        except:
+            function_call = "self.handle_key_{}()".format(event.keysym.upper())
+            eval(function_call)
+        except AttributeError:
             pass
-        draw_node(canvas, node)
-        window.wm_title(title_bar_string(node))
+        self.draw_node()
 
-    def call_mouseclick_handler(event):
-        nonlocal node
-        x, y = board_pos_from_screen_pos(event.x, event.y, node.board.boardsize)
-        node = mouseclick_handler(node, x, y)
-        draw_node(canvas, node)
-        window.wm_title(title_bar_string(node))
+    # ----- (the above makes the following work) -----
 
-    def call_opener(event):
-        nonlocal node
-        node = opener(node)
-        draw_node(canvas, node)
-        window.wm_title(title_bar_string(node))
+    def handle_key_DOWN(self):
+        try:
+            self.node = self.node.children[0]
+            self.node.print_comments()
+        except IndexError:
+            pass
 
-    def call_saver(event):
-        nonlocal node
-        node = saver(node)
-        draw_node(canvas, node)
-        window.wm_title(title_bar_string(node))
+    def handle_key_RIGHT(self):
+        self.handle_key_DOWN()
+
+    def handle_key_UP(self):
+        if self.node.parent:
+            self.node = self.node.parent
+
+    def handle_key_LEFT(self):
+        self.handle_key_UP()
+
+    def handle_key_NEXT(self):          # PageDown
+        for n in range(10):
+            try:
+                self.node = self.node.children[0]
+                self.node.print_comments()
+            except IndexError:
+                break
+
+    def handle_key_PRIOR(self):         # PageUp
+        for n in range(10):
+            if self.node.parent:
+                self.node = self.node.parent
+            else:
+                break
+
+    def handle_key_TAB(self):
+        if self.node.parent:
+            if len(self.node.parent.children) > 1:
+                index = self.node.parent.children.index(self.node)
+                if index < len(self.node.parent.children) - 1:
+                    index += 1
+                else:
+                    index = 0
+                self.node = self.node.parent.children[index]
+                self.node.print_comments()
+
+    def handle_key_BACKSPACE(self):         # Return to the main line
+        while 1:
+            if self.node.is_main_line:
+                break
+            if self.node.parent is None:
+                break
+            self.node = self.node.parent
+
+    def handle_key_HOME(self):
+        self.node = self.node.get_root_node()
+
+    def handle_key_END(self):
+        self.node = self.node.get_end_node()
+
+    def handle_key_DELETE(self):
+        if len(self.node.children) > 0:
+            ok = tkinter.messagebox.askokcancel("Delete?", "Delete this node and all of its children?")
+        else:
+            ok = True
+        if ok:
+            if self.node.parent:
+                child = self.node
+                self.node = self.node.parent
+                self.node.children.remove(child)
+                self.node.fix_main_line_status_recursive()
+            else:
+                self.node = sgf.new_tree(19)
+
+    def handle_key_D(self):
+        self.node.debug()
+
+    # Other handlers...
+
+    def opener(self, event):
+        infilename = tkinter.filedialog.askopenfilename()
+        if infilename:
+            self.open_file(infilename)
+        self.draw_node()
+
+    def saver(self, event):
+        outfilename = tkinter.filedialog.asksaveasfilename(defaultextension=".sgf")
+        if outfilename:
+            sgf.save_file(outfilename, self.node)
+            print("---> Saved: {}\n".format(outfilename))
+        self.draw_node()
+
+    def mouseclick_handler(self, event):
+        x, y = board_pos_from_screen_pos(event.x, event.y, self.node.board.boardsize)
+        result = self.node.try_move(x, y)
+        if result:
+            self.node = result
+        self.draw_node()
+
+# ---------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    print(MOTD)
 
     window = tkinter.Tk()
     window.resizable(width = False, height = False)
@@ -308,21 +294,13 @@ def main():
 
     load_graphics()
 
-    canvas = tkinter.Canvas(window, width = WIDTH, height = HEIGHT, bd = 0, highlightthickness = 0)
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = None
 
-    canvas.bind("<Key>", call_keypress_handler)
-    canvas.bind("<Button-1>", call_mouseclick_handler)
-    canvas.bind("<Control-o>", call_opener)
-    canvas.bind("<Control-s>", call_saver)
-
-    canvas.pack()
-    canvas.focus_set()
-
-    draw_node(canvas, node)
-    window.wm_title(title_bar_string(node))
+    board = BoardCanvas(window, filename, width = WIDTH, height = HEIGHT, bd = 0, highlightthickness = 0)
+    board.pack()
+    board.focus_set()
 
     window.mainloop()
-
-
-if __name__ == "__main__":
-    main()
